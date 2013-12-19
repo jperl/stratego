b6594bfde86996866c8f2564e0d733a0688d61c6
Jon Perl <perl.jonathan@gmail.com>
2013-12-18 22:40:59 -0500

    Switch to less. Add README.
    Basic front end html.

---
+++ README.md
@@ -0,0 +1,10 @@
+Stratego (Working Name)
+===========
+
+###Getting Started
+
+1. Install [meteor](http://docs.meteor.com/#quickstart) and [meteorite](http://oortcloud.github.com/meteorite/)
+
+2. Run the application (we are using the experimental meteor ui branch)
+
+		meteor --release template-engine-preview-10
--- client/main.js
+++ client/main.js
@@ -1,34 +1,26 @@
-if (Meteor.isClient) {
-    //TODO template hash tag formatter
+//TODO template hash tag formatter
 
-    Template.header.title = function () {
-        return 'We are not hiring great engineers <a href="#recruiting">#recruiting</a> <a href="#engineering">#engineering</a>';
-    };
+Template.header.title = function () {
+    return 'The organization cannot build quality products on time internally <a href="#engineering">#engineering</a> <a href="#product">#product</a>';
+};
 
-    Template.header.description = function () {
-        return 'We are not getting enough of the right people to apply. We need better candidate filtering. We do not have the proper ratios for skillsets.';
-    };
+Template.header.description = function () {
+    return 'If we do not improve this, we cannot rely on our internal resources to support the business.';
+};
 
-    Template.header.rendered = function () {
-        //TODO max characters
-        var title = new MediumEditor('.title', {
-            disableReturn: true,
-            disableToolbar: true,
-            forcePlainText: true,
-            targetBlank: true
-        });
-
-        //TODO max characters
-        var description = new MediumEditor('.description', {
-            disableReturn: true,
-            disableToolbar: true,
-            forcePlainText: true
-        });
-    };
-}
-
-if (Meteor.isServer) {
-    Meteor.startup(function () {
-        // code to run on server at startup
+Template.header.rendered = function () {
+    //TODO max characters
+    var title = new MediumEditor('.title', {
+        disableReturn: true,
+        disableToolbar: true,
+        forcePlainText: true,
+        targetBlank: true
     });
-}
+
+    //TODO max characters
+    var description = new MediumEditor('.description', {
+        disableReturn: true,
+        disableToolbar: true,
+        forcePlainText: true
+    });
+};
