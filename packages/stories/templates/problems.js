8d8e110969e71c87a753f87fd82568cd665fa00c
Jon Perl <perl.jonathan@gmail.com>
2013-12-22 18:29:21 -0500

    Setup adding new problem

--- packages/stories/templates/problems.js
+++ packages/stories/templates/problems.js
@@ -1,7 +1,19 @@
-Template.problems.events({
-    'click .new-problem': function () {
-        var id = Stories.insert({ type: Story.Type.PROBLEM });
+Template.newProblem.events({
+    'keyup .new-problem-title': _.throttle(function (event) {
+        var submitNewProblem = $('.add-new-problem');
 
+        var titleLength = $(event.target).val().length;
+        if (titleLength === 0) {
+            submitNewProblem.animate({opacity: "hide"});
+        }
+        else if (titleLength > 5) {
+            submitNewProblem.animate({opacity: "show"});
+        }
+    }, 2500),
+    'click .add-new-problem': function () {
+        var title = $('.new-problem-title').val();
+
+        var id = Stories.insert({ type: Story.Type.PROBLEM, title: title });
         Router.go('/problems/' + id);
     }
 });
