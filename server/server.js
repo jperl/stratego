2582c59e8efcdca8cf4c5048fcf85dd54d62e862
Jon Perl <perl.jonathan@gmail.com>
2014-01-13 03:38:38 -0500

    Add basic password auth commented out for deploys

---
+++ server/server.js
@@ -0,0 +1,8 @@
+//Meteor.startup(function () {
+//    WebApp.connectHandlers.stack.splice(0, 0, {
+//        route: '',
+//        handle: WebApp.__basicAuth__(function (user, pass) {
+//            return 'tequila' == user & 'foosball' == pass;
+//        })
+//    });
+//});
